5263693eb2983289f96dd97731370491bc5713aa
ftatp <ftatp5901@gmail.com>
2024-04-10 18:45:22 +0900

    Added first imgui to window

--- main.cpp
+++ main.cpp
@@ -12,23 +12,79 @@
 #include <vector>
 
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-void MakeWindow();
+const int m_screenWidth = 1280;
+const int m_screenHeight = 960;
 
-int main() 
+const float clearColor[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+IDXGISwapChain* swapChain;                  // Made by D3D11CreateDeviceAndSwapChain()
+ID3D11Device* device;                       // Made by D3D11CreateDeviceAndSwapChain()
+ID3D11DeviceContext* deviceContext;         // Made by D3D11CreateDeviceAndSwapChain()
+ID3D11RenderTargetView* renderTargetView;   // Made by device->CreateRenderTargetView(pBackBuffer, nullptr, &renderTargetView);
+
+HWND MakeWindow();
+
+void InitD3D11(HWND hWnd);
+void InitImGui(HWND hWnd);
+
+int main()
 {
-    MakeWindow();
+    MSG message = {};
+
+    HWND hWnd = MakeWindow();
+
+    ShowWindow(hWnd, SW_SHOWDEFAULT);
+    UpdateWindow(hWnd);
+
+    InitD3D11(hWnd);
+    InitImGui(hWnd);
+
+    while (message.message != WM_QUIT)
+    {
+        if (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+            continue;
+        }
+
+        ImGui_ImplDX11_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello, ImGui!");
+        ImGui::Text("This is some text");
+        ImGui::End();
+        ImGui::Render();
+
+        // Rendering
+        deviceContext->OMSetRenderTargets(1, &renderTargetView, nullptr);
+        deviceContext->ClearRenderTargetView(renderTargetView, (float*)&clearColor);
+        
+        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+        swapChain->Present(1, 0);
+    }
+
+    // Cleanup
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+    renderTargetView->Release();
+    swapChain->Release();
+    deviceContext->Release();
+    device->Release();
+    
+    DestroyWindow(hWnd);
 
     return 0;
 }
 
-void MakeWindow() 
+HWND MakeWindow() 
 {
-    int width = 1280;
-    int height = 960;
-    int canvasWidth = width / 80;
-    int canvasHeight = height / 80;
-    MSG message;
+    int canvasWidth = m_screenWidth / 80;
+    int canvasHeight = m_screenHeight / 80;
 
     WNDCLASSEX wndClass = {
         sizeof(WNDCLASSEX),      
@@ -36,35 +92,98 @@ void MakeWindow()
         WndProc, 
         0L,   
         0L,
-        GetModuleHandle(NULL),   
-        NULL,       
-        NULL,    
-        NULL, 
-        NULL,
+        GetModuleHandle(nullptr),   
+        nullptr,       
+        nullptr,    
+        nullptr, 
+        nullptr,
         L"SimpleEngine_DirectX", 
-        NULL
+        nullptr
     };
 
-    RECT windowRange = { 0, 0, 1280, 960 };
+    RECT windowRange = { 0, 0, m_screenWidth, m_screenHeight };
 
     RegisterClassEx(&wndClass);
     AdjustWindowRect(&windowRange, WS_OVERLAPPEDWINDOW, TRUE);
 
     HWND hWnd = CreateWindow(wndClass.lpszClassName, wndClass.lpszClassName, WS_OVERLAPPEDWINDOW,
-        300, 300, windowRange.right - windowRange.left, windowRange.bottom - windowRange.top,
-        NULL, NULL, wndClass.hInstance, NULL);
+        50, 50, windowRange.right - windowRange.left, windowRange.bottom - windowRange.top,
+        nullptr, nullptr, wndClass.hInstance, nullptr);
 
-    ShowWindow(hWnd, SW_SHOWDEFAULT);
-    UpdateWindow(hWnd);
-
-    while (GetMessage(&message, NULL, 0, 0)) 
-    {
-        TranslateMessage(&message);
-        DispatchMessage(&message);
-    }
+    return hWnd;
 }
 
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+void InitD3D11(HWND hWnd)
+{
+#pragma region SwapChain
+    const D3D_FEATURE_LEVEL featureLevelArray[1] = { D3D_FEATURE_LEVEL_11_0 };
+
+    DXGI_SWAP_CHAIN_DESC swapChainDescription;
+    swapChainDescription.BufferCount = 2;
+    swapChainDescription.BufferDesc.Width = m_screenWidth;
+    swapChainDescription.BufferDesc.Height = m_screenHeight;
+    swapChainDescription.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDescription.BufferDesc.RefreshRate.Numerator = 60;
+    swapChainDescription.BufferDesc.RefreshRate.Denominator = 1;
+    swapChainDescription.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDescription.OutputWindow = hWnd;
+    swapChainDescription.SampleDesc.Count = 1;
+    swapChainDescription.SampleDesc.Quality = 0;
+    swapChainDescription.Windowed = TRUE;
+    swapChainDescription.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;    // allow full-screen switching
+    swapChainDescription.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0; // D3D11_CREATE_DEVICE_DEBUG;
+
+    HRESULT result = D3D11CreateDeviceAndSwapChain(
+        nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, featureLevelArray, 1, D3D11_SDK_VERSION, &swapChainDescription, //inputs
+        &swapChain, &device, nullptr, &deviceContext); //ouputs
+
+    if (result < 0)
+    {
+        std::cout << "D3D11CreateDeviceAndSwapChain() error" << std::endl;
+        exit(-1);
+    }
+#pragma endregion
+
+#pragma region RenderTarget
+    ID3D11Texture2D* pBackBuffer = nullptr;
+    
+    swapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    
+    if (pBackBuffer != nullptr) 
+    {
+        device->CreateRenderTargetView(pBackBuffer, nullptr, &renderTargetView);
+        pBackBuffer->Release();
+    }
+    else
+    {
+        std::cout << "CreateRenderTargetView() error" << std::endl;
+        exit(-1);
+    }
+#pragma endregion
+
+#pragma region Viewport
+    // Setup viewport
+    D3D11_VIEWPORT viewport;
+    viewport.Width = 1280;
+    viewport.Height = 720;
+    viewport.MinDepth = 0.0f;
+    viewport.MaxDepth = 1.0f;
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    deviceContext->RSSetViewports(1, &viewport);
+
+#pragma endregion
+}
+
+void InitImGui(HWND hWnd)
+{
+    ImGui::CreateContext();
+    ImGui_ImplDX11_Init(device, deviceContext);
+    ImGui_ImplWin32_Init(hWnd);
+}
+
 
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) 
 {
